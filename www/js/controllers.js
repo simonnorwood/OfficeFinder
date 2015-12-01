c16612d2d333647604a43f64df6e806d84919a55
simonnorwood <212467590@HCE-JF50XZ1.logon.ds.ge.com>
2015-12-01 15:17:33 +0000

    01/12/15 15:17

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -6,22 +6,34 @@ angular.module('starter.controllers', ['ionic.utils'])
 })
 
 // api call to offices.json 
-.controller('APICtrl', function($scope, $http, $localstorage) {
+.controller('APICtrl', function($scope, $http, $localstorage, $window) {
 // Search function
   $scope.query = {}
   $scope.queryBy = '$'
 // gets the data from offices.json
  $http.get('js/offices.json').then(function(resp) {
     console.log('Success', resp);
-    $scope.offices = resp.data.office;
-    $window.localstorage(resp.data.office);
+    $scope.offices = resp.data.office;  
   }, function(err) {
     console.error('ERR', err);
     // err.status will contain the status code
   });
 
+$scope.favourties= JSON.parse($window.localStorage['fav']);
+//$scope.favourites = $window.localStorage['favs'] ? JSON.parse($window.localStorage['favs']) : []
+if($window.localStorage['fav']){
+    $scope.favorites = JSON.parse($window.localStorage['fav']);
+} else {
+    $scope.favorites = [];
+}
 
+$scope.togglefav = function(index) {
+    fav.push(index);
+    console.log(fav);
+    $window.localStorage['fav'] = JSON.stringify(fav);
+}
 })
+
 // modal controller that holds the enlarged office view and map view
 .controller('ModalCtrl', function($scope, $ionicModal) {
   $ionicModal.fromTemplateUrl('templates/modal.html', {
@@ -51,26 +63,26 @@ angular.module('starter.controllers', ['ionic.utils'])
 })
 
 // pass $index from offices.json array into new array then stringify the new array
-.controller('ToggleFav', function($scope, $window, $localstorage){
+/*.controller('ToggleFav', function($scope, $window, $localstorage){
 var fav = [];
   $scope.togglefav = function(index, liked) {
     var flag = liked;
-    if(flag==true){
+  if(flag==true){
     fav.push(index);
     console.log(fav);
-    $window.localstorage['fav'] = JSON.stringify(index);
-    $window.localstorage['flag'] = JSON.stringify(flag);
+    $window.localstorage['fav'] = JSON.stringify(fav);
+    //$window.localstorage['flag'] = JSON.stringify(flag);
     console.log($window.localstorage);
   return fav;
   }
-  else if(flag==false){
+  else{
     alert("already added");
   }
   }
-})
+})*/
 
-.controller('FavAdd', function($scope, $window, $localstorage, $http){
+/*.controller('FavAdd', function($scope, $window, $localstorage, $http){
   var fav = JSON.parse($window.localstorage['fav'] || '[]')
   console.log(fav);
   console.log($window.localstorage);
-});
+});*/
